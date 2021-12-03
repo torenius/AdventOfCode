b3029dfd2f6f89b1d2c37d8ed50a8e39630370af
Tommy Torenius <tommy_torenius@hotmail.com>
2021-12-03 10:48:12 +0100

    Refactor Day01

---
+++ AOC2021/01/Day01A.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace AOC2021._01
+{
+    public class Day01A : Day
+    {
+        public override void Run()
+        {
+            var input = GetInputAsInt();
+            var count = 0;
+            for (var i = 1; i < input.Length; i++)
+            {
+                if (input[i - 1] < input[i])
+                {
+                    count++;
+                }
+            }
+            
+            Console.WriteLine(count);
+        }
+    }
+}
---
+++ AOC2021/01/Day01B.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace AOC2021._01
+{
+    public class Day01B : Day
+    {
+        public override void Run()
+        {
+            var input = GetInputAsInt();
+            var count = 0;
+            for (var i = 0; i < input.Length - 3; i++)
+            {
+                var a = input[i] + input[i + 1] + input[i + 2];
+                var b = input[i + 1] + input[i + 2] + input[i + 3];
+
+                if (a < b)
+                {
+                    count++;
+                }
+            }
+        
+            Console.WriteLine(count);
+        }
+    }
+}

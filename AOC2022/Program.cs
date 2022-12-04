9aa2fca72218dfc55a06dc039fe7f13c007810dc
Tommy Torenius <tommy.torenius@bokadirekt.se>
2022-12-04 13:43:44 +0100

    Colors

--- AOC2022/Program.cs
+++ AOC2022/Program.cs
@@ -4,14 +4,18 @@ using AOC2022._02;
 using AOC2022._03;
 using AOC2022._04;
 
+Console.ForegroundColor = ConsoleColor.DarkRed;
 var day = new Day04B();
 
+Console.ForegroundColor = ConsoleColor.Green;
 var stop = Stopwatch.StartNew();
 var result = day.Run();
 stop.Stop();
 
 Console.WriteLine();
+Console.ForegroundColor = ConsoleColor.DarkGreen;
 Console.WriteLine(result);
 Console.WriteLine();
 
+Console.ForegroundColor = ConsoleColor.White;
 Console.WriteLine(stop.Elapsed);

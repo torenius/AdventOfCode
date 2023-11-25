77d306890d263f02a22d54b7dd41b2399842391f
Tommy Torenius <tommy.torenius@bokadirekt.se>
2023-11-25 21:44:18 +0100

    Part 1 for Day 17 and Day 18

---
+++ AOC2022/17/Day17A.cs
@@ -0,0 +1,276 @@
+﻿using System.Drawing;
+
+namespace AOC2022._17;
+
+public class Day17A : Day
+{
+    private char[] Gas;
+    private int GasIndex = -1;
+    
+    protected override string Run()
+    {
+        var input = ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>";
+        input = GetInputAsString().Trim('\n');
+
+        Gas = input.ToCharArray();
+        
+        var cave = new bool[9000, 7];
+
+        var rocks = new List<Shape>();
+        
+        // Add 2020
+        for (var i = 0; i < 404; i++)
+        {
+            rocks.Add(new Minus());
+            rocks.Add(new Plus());
+            rocks.Add(new J());
+            rocks.Add(new I());
+            rocks.Add(new Square());
+        }
+        // Add the last 2
+        rocks.Add(new Minus());
+        rocks.Add(new Plus());
+
+        var minY = cave.GetLength(0);
+
+        foreach (var rock in rocks)
+        {
+            rock.BottomLeftCoordinate = new Point(2, minY - 4);
+
+            do
+            {
+                //Print(cave, rock.TopY, rock);
+                var gas = GetNextGas();
+                if (gas == '>')
+                {
+                    rock.MoveRight(cave);
+                }
+                else
+                {
+                    rock.MoveLeft(cave);
+                }
+                //Print(cave, rock.TopY, rock);
+            }while(!rock.MoveDown(cave));
+            
+            
+            rock.Stopped(cave);
+            minY = Math.Min(minY, rock.TopY);
+            //Print(cave, rock.TopY);
+        }
+        
+        //Print(cave, minY);
+
+        return "" + (cave.GetLength(0) - minY);
+    }
+
+    private static void Print(bool[,] cave, int from, Shape? rock = null)
+    {
+        var footprint = rock != null ? rock.GetFootprint() : new List<(int y, int x)>();
+        for (var y = from; y < cave.GetLength(0); y++)
+        {
+            var row = ("" + (cave.GetLength(0) - y)).PadLeft(4, '0') + " ";
+            for (var x = 0; x < cave.GetLength(1); x++)
+            {
+                if (footprint.Contains((y, x)))
+                {
+                    row += "¤";
+                }
+                else
+                {
+                    row += cave[y, x] ? "#" : ".";    
+                }
+            }
+            Console.WriteLine(row);
+        }
+        Console.WriteLine();
+    }
+
+    private char GetNextGas()
+    {
+        if (GasIndex + 1 == Gas.Length)
+        {
+            GasIndex = -1;
+        }
+
+        GasIndex++;
+
+        return Gas[GasIndex];
+    }
+
+    private abstract class Shape
+    {
+        public Point BottomLeftCoordinate { get; set; }
+        public abstract int TopY { get; }
+
+        public List<(int Y, int X)> GetFootprint()
+        {
+            return GetFootprint(BottomLeftCoordinate.Y, BottomLeftCoordinate.X);
+        }
+        
+        public abstract List<(int Y, int X)> GetFootprint(int y, int x);
+
+        public void MoveRight(bool[,] cave)
+        {
+            var y = BottomLeftCoordinate.Y;
+            var x = BottomLeftCoordinate.X + 1;
+
+            if (DoRockCollide(cave, y, x)) return;
+
+            BottomLeftCoordinate = new Point(x, y);
+        }
+        
+        public void MoveLeft(bool[,] cave)
+        {
+            var y = BottomLeftCoordinate.Y;
+            var x = BottomLeftCoordinate.X - 1;
+            
+            if (DoRockCollide(cave, y, x)) return;
+
+            BottomLeftCoordinate = new Point(x, y);
+        }
+        
+        public bool MoveDown(bool[,] cave)
+        {
+            var y = BottomLeftCoordinate.Y + 1;
+            var x = BottomLeftCoordinate.X;
+
+            if (y >= cave.GetLength(0)) return true;
+
+            if (DoRockCollide(cave, y, x)) return true;
+
+            BottomLeftCoordinate = new Point(x, y);
+
+            return false;
+        }
+
+        private bool DoRockCollide(bool[,] cave, int y, int x)
+        {
+            var caveWidth = cave.GetLength(1);
+            foreach (var footprint in GetFootprint(y, x))
+            {
+                if (footprint.X >= caveWidth || footprint.X < 0)
+                {
+                    return true;
+                }
+            
+                if (cave[footprint.Y, footprint.X])
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public void Stopped(bool[,] cave)
+        {
+            foreach (var footprint in GetFootprint())
+            {
+                cave[footprint.Y, footprint.X] = true;
+            }
+        }
+    }
+
+    private class Minus : Shape
+    {
+        public override int TopY => BottomLeftCoordinate.Y;
+        public override List<(int Y, int X)> GetFootprint(int y, int x)
+        {
+            return new List<(int Y, int X)>
+            {
+                /*
+                 * ####
+                 */
+                {(y, x)},
+                {(y, x + 1)},
+                {(y, x + 2)},
+                {(y, x + 3)}
+            };
+        }
+    }
+
+    private class Plus : Shape
+    {
+        public override int TopY => BottomLeftCoordinate.Y - 2;
+        
+        public override List<(int Y, int X)> GetFootprint(int y, int x)
+        {
+            return new List<(int Y, int X)>
+            {
+                /*
+                 * .#.
+                 * ###
+                 * .#.
+                 */
+                {(y - 2, x + 1)},
+                {(y - 1, x)},
+                {(y - 1, x + 1)},
+                {(y - 1, x + 2)},
+                {(y, x + 1)}
+            };
+        }
+    }
+
+    private class J : Shape
+    {
+        public override int TopY => BottomLeftCoordinate.Y - 2;
+        public override List<(int Y, int X)> GetFootprint(int y, int x)
+        {
+            return new List<(int Y, int X)>
+            {
+                /*
+                 * ..#
+                 * ..#
+                 * ###
+                 */
+                {(y, x)},
+                {(y, x + 1)},
+                {(y, x + 2)},
+                {(y - 1, x + 2)},
+                {(y - 2, x + 2)}
+            };
+        }
+    }
+    
+    private class I : Shape
+    {
+        public override int TopY => BottomLeftCoordinate.Y - 3;
+        
+        public override List<(int Y, int X)> GetFootprint(int y, int x)
+        {
+            return new List<(int Y, int X)>
+            {
+                /*
+                 * #
+                 * #
+                 * #
+                 * #
+                 */
+                {(y, x)},
+                {(y - 1, x)},
+                {(y - 2, x)},
+                {(y - 3, x)}
+            };
+        }
+    }
+
+    private class Square : Shape
+    {
+        public override int TopY => BottomLeftCoordinate.Y - 1;
+        
+        public override List<(int Y, int X)> GetFootprint(int y, int x)
+        {
+            return new List<(int Y, int X)>
+            {
+                /*
+                 * ##
+                 * ##
+                 */
+                {(y, x)},
+                {(y, x + 1)},
+                {(y - 1, x)},
+                {(y - 1, x + 1)}
+            };
+        }
+    }
+}
---
+++ AOC2022/18/Day18A.cs
@@ -0,0 +1,147 @@
+﻿using System.Numerics;
+
+namespace AOC2022._18;
+
+public class Day18A : Day
+{
+    protected override string Run()
+    {
+        var input = GetInputAsStringArray();
+        var cubes = input.Select(x => new Cube(x)).ToList();
+
+        
+        for (var a = 0; a < cubes.Count - 1; a++)
+        {
+            var cubeA = cubes[a];
+            for (var b = a + 1; b < cubes.Count; b++)
+            {
+                var cubeB = cubes[b];
+                cubeA.CompareCube(cubeB);
+            }
+        }
+        
+        var exposedCount = cubes.Sum(cube => cube.NumberOfExposedSides);
+
+        return "" + exposedCount;
+    }
+
+    private class Cube
+    {
+        public Cube(string input)
+        {
+            var coordinates = input.Split(",");
+            var x = coordinates[0].ToInt();
+            var y = coordinates[1].ToInt();
+            var z = coordinates[2].ToInt();
+
+            FrontBottomLeft = new Vector3(x, y, z);
+            FrontTopLeft = new Vector3(x, y + 1, z);
+            FrontBottomRight = new Vector3(x + 1, y, z);
+            FrontTopRight = new Vector3(x + 1, y + 1, z);
+
+            BackBottomLeft = new Vector3(x, y, z + 1);
+            BackTopLeft = new Vector3(x, y + 1, z + 1);
+            BackBottomRight = new Vector3(x + 1, y, z + 1);
+            BackTopRight = new Vector3(x + 1, y + 1, z + 1);
+        }
+
+        public Vector3 FrontBottomLeft { get; set; }
+        public Vector3 FrontTopLeft { get; set; }
+        public Vector3 FrontBottomRight { get; set; }
+        public Vector3 FrontTopRight { get; set; }
+        
+        public Vector3 BackBottomLeft { get; set; }
+        public Vector3 BackTopLeft { get; set; }
+        public Vector3 BackBottomRight { get; set; }
+        public Vector3 BackTopRight { get; set; }
+
+        public Cube? FrontConnectedWith { get; set; }
+        public Cube? BackConnectedWith { get; set; }
+        public Cube? LeftConnectedWith { get; set; }
+        public Cube? RightConnectedWith { get; set; }
+        public Cube? TopConnectedWith { get; set; }
+        public Cube? BottomConnectedWith { get; set; }
+
+        public int NumberOfExposedSides =>
+            (FrontConnectedWith is null ? 1 : 0) +
+            (BackConnectedWith is null ? 1 : 0) +
+            (LeftConnectedWith is null ? 1 : 0) +
+            (RightConnectedWith is null ? 1 : 0) +
+            (TopConnectedWith is null ? 1 : 0) +
+            (BottomConnectedWith is null ? 1 : 0);
+
+        public void CompareCube(Cube other)
+        {
+            // This Top, Other Bottom
+            if (TopConnectedWith is null && other.BottomConnectedWith is null
+                                && FrontTopLeft.Equals(other.FrontBottomLeft)
+                                && FrontTopRight.Equals(other.FrontBottomRight)
+                                && BackTopLeft.Equals(other.BackBottomLeft)
+                                && BackTopRight.Equals(other.BackBottomRight))
+            {
+                TopConnectedWith = other;
+                other.BottomConnectedWith = this;
+                return;
+            }
+            
+            // This bottom, Other Top
+            if (BottomConnectedWith is null && other.TopConnectedWith is null
+                                            && other.FrontTopLeft.Equals(FrontBottomLeft)
+                                            && other.FrontTopRight.Equals(FrontBottomRight)
+                                            && other.BackTopLeft.Equals(BackBottomLeft)
+                                            && other.BackTopRight.Equals(BackBottomRight))
+            {
+                BottomConnectedWith = other;
+                other.TopConnectedWith = this;
+                return;
+            }
+            
+            // This Left, Other Right
+            if (LeftConnectedWith is null && other.RightConnectedWith is null
+                                          && BackTopLeft.Equals(other.BackTopRight)
+                                          && FrontTopLeft.Equals(other.FrontTopRight)
+                                          && BackBottomLeft.Equals(other.BackBottomRight)
+                                          && FrontBottomLeft.Equals(other.FrontBottomRight))
+            {
+                LeftConnectedWith = other;
+                other.RightConnectedWith = this;
+                return;
+            }
+            
+            // This Right, Other Left
+            if (RightConnectedWith is null && other.LeftConnectedWith is null
+                                           && other.BackTopLeft.Equals(BackTopRight)
+                                           && other.FrontTopLeft.Equals(FrontTopRight)
+                                           && other.BackBottomLeft.Equals(BackBottomRight)
+                                           && other.FrontBottomLeft.Equals(FrontBottomRight))
+            {
+                RightConnectedWith = other;
+                other.LeftConnectedWith = this;
+                return;
+            }
+            
+            // This Back, Other Front
+            if (BackConnectedWith is null && other.FrontConnectedWith is null
+                                          && BackTopLeft.Equals(other.FrontTopLeft)
+                                          && BackTopRight.Equals(other.FrontTopRight)
+                                          && BackBottomLeft.Equals(other.FrontBottomLeft)
+                                          && BackBottomRight.Equals(other.FrontBottomRight))
+            {
+                BackConnectedWith = other;
+                other.FrontConnectedWith = this;
+                return;
+            }
+            
+            // This Front, Other Back
+            if (FrontConnectedWith is null && other.BackConnectedWith is null
+                                           && other.BackTopLeft.Equals(FrontTopLeft)
+                                           && other.BackTopRight.Equals(FrontTopRight)
+                                           && other.BackBottomLeft.Equals(FrontBottomLeft)
+                                           && other.BackBottomRight.Equals(FrontBottomRight))
+            {
+                FrontConnectedWith = other;
+                other.BackConnectedWith = this;
+            }
+        }
+    }
+}

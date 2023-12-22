ff715afdca5c6cdf19eca96a9f709bd031051dd0
Tommy Torenius <tommy.torenius@bokadirekt.se>
2023-12-22 23:35:55 +0100

    2023 Day18

---
+++ AOC2023/18/Day18A.cs
@@ -0,0 +1,150 @@
+﻿using System.Drawing;
+
+namespace AOC2023._18;
+
+public class Day18A : Day
+{
+    protected override object Run()
+    {
+        var input = GetInputAsStringArray();
+        var instructions = input.Select(x => new DigInstruction(x)).ToList();
+        var holes = new List<Point>();
+
+        var current = new Point(0, 0);
+        holes.Add(current);
+        foreach (var instruction in instructions)
+        {
+            var points = instruction.Direction switch
+            {
+                "U" => GetPointsOnLine(current, 0, -1, instruction.Steps),
+                "D" => GetPointsOnLine(current, 0, 1, instruction.Steps),
+                "L" => GetPointsOnLine(current, -1, 0, instruction.Steps),
+                "R" => GetPointsOnLine(current, 1, 0, instruction.Steps),
+            };
+            current = points.Last();
+            holes.AddRange(points);
+        }
+        
+        //Helper.Print(holes);
+
+        var matrix = holes.ToCharArray();
+        var inside = FindPointInside(matrix);
+        Expand(matrix, inside.Y, inside.X);
+        
+        //Console.WriteLine(matrix.ToText());
+        
+        var sum = 0;
+        for (var y = 0; y < matrix.GetLength(0); y++)
+        {
+            for (var x = 0; x < matrix.GetLength(1); x++)
+            {
+                if (matrix[y, x] == '#')
+                {
+                    sum++;
+                }
+            }
+        }
+        
+        return sum;
+    }
+
+    private static List<Point> GetPointsOnLine(Point point, int xDiff, int yDiff, int steps)
+    {
+        var result = new List<Point>();
+        for (var i = 1; i <= steps; i++)
+        {
+            result.Add(point with { X = point.X + xDiff * i, Y = point.Y + yDiff * i});
+        }
+
+        return result;
+    }
+
+    private static void Expand(char[,] input, int y, int x)
+    {
+        var start = new Point(x, y);
+        foreach (var point in Helper.BreadthFirst(start, (point) => GetChildren(input, point)))
+        {
+            input[point.Y, point.X] = '#';
+        }
+        
+    }
+
+    private static IEnumerable<Point> GetChildren(char[,] input, Point point)
+    {
+        if (point.Y - 1 >= 0 && input[point.Y - 1, point.X] == '.')
+        {
+            yield return point with {Y = point.Y - 1};
+        }
+        
+        if (point.Y + 1 < input.GetLength(0) && input[point.Y + 1, point.X] == '.')
+        {
+            yield return point with {Y = point.Y + 1};
+        }
+        
+        if (point.X - 1 >= 0 && input[point.Y, point.X - 1] == '.')
+        {
+            yield return point with {X = point.X - 1};
+        }
+        
+        if (point.X + 1 < input.GetLength(1) && input[point.Y, point.X + 1] == '.')
+        {
+            yield return point with {X = point.X + 1};
+        }
+    }
+    
+
+    private static (int Y, int X) FindPointInside(char[,] input)
+    {
+        for (var y = 0; y < input.GetLength(0); y++)
+        {
+            for (var x = 0; x < input.GetLength(1); x++)
+            {
+                if (input[y, x] == '.')
+                {
+                    var count = 0;
+                    for (var z = x - 1; z >= 0; z--)
+                    {
+                        if (input[y, z] == '#')
+                        {
+                            count++;
+                        }
+                    }
+
+                    if (count % 2 == 1)
+                    {
+                        count = 0;
+                        for (var z = y - 1; z >= 0; z--)
+                        {
+                            if (input[z, x] == '#')
+                            {
+                                count++;
+                            }
+                        }
+
+                        if (count % 2 == 1)
+                        {
+                            return (y, x);
+                        }
+                    }
+                }
+            }
+        }
+
+        return (-1, -1);
+    }
+    
+    private class DigInstruction
+    {
+        public DigInstruction(string input)
+        {
+            var temp = input.Split(" ");
+            Direction = temp[0];
+            Steps = temp[1].ToInt();
+            HexColor = temp[2];
+        }
+
+        public string Direction { get; set; }
+        public int Steps { get; set; }
+        public string HexColor { get; set; }
+    }
+}
---
+++ AOC2023/18/Day18B.cs
@@ -0,0 +1,95 @@
+﻿using System.Drawing;
+
+namespace AOC2023._18;
+
+public class Day18B : Day
+{
+    protected override object Run()
+    {
+        var input = GetInputAsStringArray();
+        var instructions = input.Select(x => new DigInstruction(x)).ToList();
+        var polygon = new List<Point>();
+        
+        var current = new Point(0, 0);
+        foreach (var instruction in instructions)
+        {
+            current = instruction.Direction switch
+            {
+                "U" => current with {Y = current.Y - instruction.Steps},
+                "D" => current with {Y = current.Y + instruction.Steps},
+                "L" => current with {X = current.X - instruction.Steps},
+                "R" => current with {X = current.X + instruction.Steps},
+            };
+            polygon.Add(current);
+        }
+
+        return Area(polygon);
+    }
+
+    private static long Area(IReadOnlyList<Point> polygon)
+    {
+        var area = ShoelaceArea(polygon);
+        var boundaryPoints = PerimeterLength(polygon);
+        var interiorPoints = InteriorPoints(area, boundaryPoints);
+        return interiorPoints + boundaryPoints;
+    }
+
+    private static long ShoelaceArea(IReadOnlyList<Point> polygon)
+    {
+        long area = 0;
+        var length = polygon.Count;
+        for (var i = 0; i < length; i++)
+        {
+            var next = (i + 1) % length;
+            var previous = (i - 1 + length) % length;
+            area += (long)polygon[i].X * (polygon[next].Y - polygon[previous].Y);
+        }
+
+        return Math.Abs(area) / 2;
+    }
+
+    // Boundary points
+    private static long PerimeterLength(IReadOnlyList<Point> polygon)
+    {
+        long perimeter = 0;
+        var length = polygon.Count;
+        for (var i = 0; i < length; i++)
+        {
+            var next = (i + 1) % length;
+            perimeter += (long)Math.Abs(polygon[next].X - polygon[i].X) + Math.Abs(polygon[next].Y - polygon[i].Y);
+        }
+        
+        return perimeter;
+    }
+
+    private static long InteriorPoints(long area, long boundaryPoints)
+    {
+        return area - (boundaryPoints / 2) + 1;
+    }
+
+    private class DigInstruction
+    {
+        public DigInstruction(string input)
+        {
+            var temp = input.Split(" ");
+            Direction = temp[0];
+            Steps = temp[1].ToInt();
+            
+            // Part 2
+            //return;
+            var hexValue = temp[2].Trim('(', ')');
+            var hexColor = hexValue[1..^1];
+            Direction = hexValue[^1] switch
+            {
+                '0' => "R",
+                '1' => "D",
+                '2' => "L",
+                '3' => "U"
+            };
+            Steps = Convert.ToInt32("0x" + hexColor, 16);
+        }
+
+        public string Direction { get; set; }
+        public int Steps { get; set; }
+    }
+}

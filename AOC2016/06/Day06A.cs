19714658905c5b53e0bf2d6f0285cb317f69bada
Tommy Torenius <tommy_torenius@hotmail.com>
2024-11-24 20:49:42 +0100

    2016 Day06

---
+++ AOC2016/06/Day06A.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+namespace AOC2016._06;
+
+public class Day06A : Day
+{
+    protected override object Run()
+    {
+        var input = GetInputAsStringArray();
+        var common = new Dictionary<int, Dictionary<char, int>>();
+        foreach (var row in input)
+        {
+            for (var i = 0; i < row.Length; i++)
+            {
+                if (!common.ContainsKey(i))
+                {
+                    common.Add(i, new Dictionary<char, int>());
+                }
+
+                if (!common[i].TryAdd(row[i], 1))
+                {
+                    common[i][row[i]] += 1;
+                }
+            }
+        }
+
+        var sb = new StringBuilder();
+        foreach (var kvp in common.OrderBy(x => x.Key))
+        {
+            sb.Append(kvp.Value.MaxBy(x => x.Value).Key);
+        }
+
+        return sb.ToString();
+    }
+}
--- AOC2016/Program.cs
+++ AOC2016/Program.cs
@@ -3,5 +3,6 @@ using AOC2016._02;
 using AOC2016._03;
 using AOC2016._04;
 using AOC2016._05;
+using AOC2016._06;
 
-new Day05B().Start();
+new Day06B().Start();
